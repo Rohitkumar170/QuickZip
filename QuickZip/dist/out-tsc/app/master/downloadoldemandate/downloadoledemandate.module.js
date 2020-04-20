e7bd852da24e128bf56b89d018817f9867f4fb97
AvantikaTyagi4 <tyagiavantika2@gmail.com>
2020-04-20 21:17:49 +0530

    Ashwini changes

--- QuickZip/dist/out-tsc/app/master/downloadoldemandate/downloadoledemandate.module.js
+++ QuickZip/dist/out-tsc/app/master/downloadoldemandate/downloadoledemandate.module.js
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { DownloadoldemandateComponent } from './downloadoldemandate.component';
 import { CommonModule } from '@angular/common';
 import { DownloadoledemandateRoutingModule } from './downloadoledemandate-routing.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { DownloadoldemandateService } from '../../Services/downloadoldemandate/downloadoldemandate.service';
 var DownloadoledemandateModule = /** @class */ (function () {
     function DownloadoledemandateModule() {
     }
@@ -11,8 +13,11 @@ var DownloadoledemandateModule = /** @class */ (function () {
             declarations: [DownloadoldemandateComponent],
             imports: [
                 CommonModule,
-                DownloadoledemandateRoutingModule
-            ]
+                DownloadoledemandateRoutingModule,
+                ReactiveFormsModule,
+                FormsModule
+            ],
+            providers: [DownloadoldemandateService]
         })
     ], DownloadoledemandateModule);
     return DownloadoledemandateModule;
