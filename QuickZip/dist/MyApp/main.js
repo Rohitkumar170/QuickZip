7c0ea28743c178c2235b5a1038b2c883abac0156
Paras <Paraspanday@123>
2020-04-21 10:40:34 +0530

    Paras work

--- QuickZip/dist/MyApp/main.js
+++ QuickZip/dist/MyApp/main.js
@@ -1365,7 +1365,7 @@ _angular_platform_browser__WEBPACK_IMPORTED_MODULE_3__["platformBrowser"]().boot
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! E:\projects\NewQuickzip\QuickZip\ClientApp\main.ts */"./ClientApp/main.ts");
+module.exports = __webpack_require__(/*! D:\New QuickZip Angular Project\QuickZip\ClientApp\main.ts */"./ClientApp/main.ts");
 
 
 /***/ })
