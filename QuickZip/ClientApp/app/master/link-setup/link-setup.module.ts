39c1598588ccc5cc81449053ef16801fc804a9ba
vimalakgec <45147202+vimalakgec@users.noreply.github.com>
2020-04-27 21:05:40 +0530

    link setup

--- QuickZip/ClientApp/app/master/link-setup/link-setup.module.ts
+++ QuickZip/ClientApp/app/master/link-setup/link-setup.module.ts
@@ -1,13 +1,20 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { LinkSetupComponent } from './link-setup.component';
 import { LinkSetupRoutingModule } from './link-setup-routing.module';
+//import { LinkSetupService } from 'ClientApp/app/Services/link-setup/link-setup.service';
 
 @NgModule({
     declarations: [LinkSetupComponent],
     imports: [
         CommonModule,
-        LinkSetupRoutingModule
-    ]
+        LinkSetupRoutingModule, FormsModule, ReactiveFormsModule
+    ],
+    //providers: [LinkSetupService, { provide: 'BASE_URL', useFactory: getBaseUrl }]
 })
 export class LinkSetupModule { }
+//export function getBaseUrl() {
+//    return document.getElementsByTagName('base')[0].href;
+//}
